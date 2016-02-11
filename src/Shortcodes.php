c04c17a6165e6a3c238cf93d91e7701c515ae68c
Remco Tolsma <info@remcotolsma.nl>
2016-02-11 14:44:18 +0100

    Happy 2016.

--- src/Shortcodes.php
+++ src/Shortcodes.php
@@ -3,7 +3,7 @@
 /**
  * Title: s2Member shortcodes
  * Description:
- * Copyright: Copyright (c) 2005 - 2015
+ * Copyright: Copyright (c) 2005 - 2016
  * Company: Pronamic
  *
  * @author Remco Tolsma

061d83de39c0cec7f7a5f59a7de726897f5d6644
Remco Tolsma <info@remcotolsma.nl>
2017-01-25 14:36:56 +0100

    Getting ready for version 1.2.6.

--- src/Extension.php
+++ src/Extension.php
@@ -3,11 +3,11 @@
 /**
  * Title: s2Member extension
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2017
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.2.2
+ * @version 1.2.6
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Extensions_S2Member_Extension {

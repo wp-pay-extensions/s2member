423957e950cb3ddb2f0694c83420c8a780ad151f
Remco Tolsma <info@remcotolsma.nl>
2017-12-12 16:24:33 +0100

    Fixing Travis CI errors.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,7 @@ module.exports = function( grunt ) {
 				]
 			},
 			options: {
+				bin: 'vendor/bin/phpcs',
 				standard: 'phpcs.ruleset.xml',
 				showSniffCodes: true
 			}
@@ -38,6 +39,7 @@ module.exports = function( grunt ) {
 				dir: 'src'
 			},
 			options: {
+				bin: 'vendor/bin/phpmd',
 				exclude: 'node_modules',
 				reportFormat: 'xml',
 				rulesets: 'phpmd.ruleset.xml'

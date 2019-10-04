bfa7b186ede11fcfbb45633dc5478518179a0632
Remco Tolsma <info@remcotolsma.nl>
2019-10-04 16:19:56 +0200

    Revert "Move from WP_Error to exceptions."
    This reverts commit 2134db8904f1eba9590267fce60814c4f64ba25d.

--- src/Shortcodes.php
+++ src/Shortcodes.php
@@ -4,6 +4,7 @@ namespace Pronamic\WordPress\Pay\Extensions\S2Member;
 
 use c_ws_plugin__s2member_list_servers;
 use Pronamic\WordPress\Pay\Core\Util as Core_Util;
+use Pronamic\WordPress\Pay\Exceptions\PayException;
 use Pronamic\WordPress\Pay\Plugin;
 use Pronamic\WordPress\Pay\Util as Pay_Util;
 
@@ -229,16 +230,7 @@ class Shortcodes {
 		}
 
 		// Start.
-		try {
-			$payment = Plugin::start( $config_id, $gateway, $data, $data->get_payment_method() );
-		} catch ( \Pronamic\WordPress\Pay\PayException $e ) {
-			// Set error message.
-			$this->error[ $index ] = array( Plugin::get_default_error_message() );
-
-			$this->error[ $index ][] = $e->get_message();
-
-			$payment = $e->get_payment();
-		}
+		$payment = Plugin::start( $config_id, $gateway, $data, $data->get_payment_method() );
 
 		update_post_meta( $payment->get_id(), '_pronamic_payment_s2member_period', $data->get_period() );
 		update_post_meta( $payment->get_id(), '_pronamic_payment_s2member_level', $data->get_level() );
@@ -255,7 +247,16 @@ class Shortcodes {
 			update_post_meta( $payment->get_subscription_id(), '_pronamic_subscription_s2member_ccaps', $data->get_ccaps() );
 		}
 
-		if ( ! isset( $this->error[ $index ] ) ) {
+		$error = $gateway->get_error();
+
+		if ( is_wp_error( $error ) ) {
+			// Set error message.
+			$this->error[ $index ] = array( Plugin::get_default_error_message() );
+
+			foreach ( $error->get_error_messages() as $message ) {
+				$this->error[ $index ][] = $message;
+			}
+		} else {
 			// Redirect.
 			$gateway->redirect( $payment );
 		}
